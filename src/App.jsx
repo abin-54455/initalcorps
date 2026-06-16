353475c591bc23ee70b4b65ab547d49741dbf84f
Abin <chandranabin741@gmail.com>
2026-06-16 10:12:14 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<div>
+</div>
+  )
+}
+
+export default App
